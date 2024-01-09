556aece6c8898e6a2a9f63336b72a75a9fbfcb94
n2ak <n2akajdaou@gmail.com>
2024-01-09 02:35:40 +0000

    relative imports

--- encoder.py
+++ encoder.py
@@ -1,13 +1,13 @@
 import torch.nn as nn
-from attention import Embedding, MultiHeadAttention, FeedForward
+from .attention import Embedding, MultiHeadAttention, FeedForward
 
 
 class Encoder(nn.Module):
-    def __init__(self, N, vocab_size, n_embeddings, time, n_heads, ff_scale) -> None:
+    def __init__(self, N, vocab_size, n_embeddings, time, n_heads, ff_scale, device) -> None:
         super().__init__()
         self.embedding = Embedding(vocab_size, n_embeddings, time)
         self.encoder_blocks = nn.ModuleList(
-            [EncoderBlock(n_heads, n_embeddings, ff_scale)
+            [EncoderBlock(n_heads, n_embeddings, ff_scale, device)
              for _ in range(N)]
         )
 
@@ -19,10 +19,11 @@ class Encoder(nn.Module):
 
 
 class EncoderBlock(nn.Module):
-    def __init__(self, n_heads, n_embeddings, ff_scale) -> None:
+    def __init__(self, n_heads, n_embeddings, ff_scale, device) -> None:
         super().__init__()
         self.ln1 = nn.LayerNorm(n_embeddings)
-        self.mha = MultiHeadAttention(n_heads, n_embeddings, masked=False)
+        self.mha = MultiHeadAttention(
+            n_heads, n_embeddings, device, masked=False)
 
         self.ln2 = nn.LayerNorm(n_embeddings)
         self.ff = FeedForward(n_embeddings, ff_scale)
--- gpt.py
+++ gpt.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 import torch
 from torch import nn
-from decoder import Decoder
+from .decoder import Decoder
 
 
 @dataclass
